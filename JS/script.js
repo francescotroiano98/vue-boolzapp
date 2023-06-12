6a846ae48b49ddd2e1c900e90307de175c61b9db
francescotroiano98 <francesco-troiano-98@libero.it>
2023-06-12 12:48:32 +0200

    add correct time to the messages

--- JS/script.js
+++ JS/script.js
@@ -14,6 +14,8 @@ createApp({
   data() {
     return {
       searchTerm: '',
+
+      moment: moment,
       
       contacts: [
         {
@@ -22,17 +24,17 @@ createApp({
             visible: true,
             messages: [
                 {
-                    date: '10/01/2020 15:30:55',
+                    date: '2020/01/10 15:30:55',
                     message: 'Hai portato a spasso il cane?',
                     status: 'sent'
                 },
                 {
-                    date: '10/01/2020 15:50:00',
+                    date: '2020/01/10 15:50:00',
                     message: 'Ricordati di stendere i panni',
                     status: 'sent'
                 },
                 {
-                    date: '10/01/2020 16:15:22',
+                    date: '2020/01/10 16:15:22',
                     message: 'Tutto fatto!',
                     status: 'received'
                 }
@@ -44,17 +46,17 @@ createApp({
             visible: true,
             messages: [
                 {
-                    date: '20/03/2020 16:30:00',
+                    date: '2020/03/20 16:30:00',
                     message: 'Ciao come stai?',
                     status: 'sent'
                 },
                 {
-                    date: '20/03/2020 16:30:55',
+                    date: '2020/03/20 16:30:55',
                     message: 'Bene grazie! Stasera ci vediamo?',
                     status: 'received'
                 },
                 {
-                    date: '20/03/2020 16:35:00',
+                    date: '2020/03/20 16:35:00',
                     message: 'Mi piacerebbe ma devo andare a fare la spesa.',
                     status: 'sent'
                 }
@@ -66,17 +68,17 @@ createApp({
             visible: true,
             messages: [
                 {
-                    date: '28/03/2020 10:10:40',
+                    date: '2020/03/28 10:10:40',
                     message: 'La Marianna va in campagna',
                     status: 'received'
                 },
                 {
-                    date: '28/03/2020 10:20:10',
+                    date: '2020/03/28 10:20:10',
                     message: 'Sicuro di non aver sbagliato chat?',
                     status: 'sent'
                 },
                 {
-                    date: '28/03/2020 16:15:22',
+                    date: '2020/03/28 16:15:22',
                     message: 'Ah scusa!',
                     status: 'received'
                 }
@@ -88,12 +90,12 @@ createApp({
             visible: true,
             messages: [
                 {
-                    date: '10/01/2020 15:30:55',
+                    date: '2020/01/10 15:30:55',
                     message: 'Lo sai che ha aperto una nuova pizzeria?',
                     status: 'sent'
                 },
                 {
-                    date: '10/01/2020 15:50:00',
+                    date: '2020/01/10 15:50:00',
                     message: 'Si, ma preferirei andare al cinema',
                     status: 'received'
                 }
@@ -105,12 +107,12 @@ createApp({
             visible: true,
             messages: [
                 {
-                    date: '10/01/2020 15:30:55',
+                    date: '2020/01/10 15:30:55',
                     message: 'Ricordati di chiamare la nonna',
                     status: 'sent'
                 },
                 {
-                    date: '10/01/2020 15:50:00',
+                    date: '2020/01/10 15:50:00',
                     message: 'Va bene, stasera la sento',
                     status: 'received'
                 }
@@ -122,17 +124,17 @@ createApp({
             visible: true,
             messages: [
                 {
-                    date: '10/01/2020 15:30:55',
+                    date: '2020/01/10 15:30:55',
                     message: 'Ciao Claudia, hai novità?',
                     status: 'sent'
                 },
                 {
-                    date: '10/01/2020 15:50:00',
+                    date: '2020/01/10 15:50:00',
                     message: 'Non ancora',
                     status: 'received'
                 },
                 {
-                    date: '10/01/2020 15:51:00',
+                    date: '2020/01/10 15:51:00',
                     message: 'Nessuna nuova, buona nuova',
                     status: 'sent'
                 }
@@ -144,12 +146,12 @@ createApp({
             visible: true,
             messages: [
                 {
-                    date: '10/01/2020 15:30:55',
+                    date: '2020/01/10 15:30:55',
                     message: 'Fai gli auguri a Martina che è il suo compleanno!',
                     status: 'sent'
                 },
                 {
-                    date: '10/01/2020 15:50:00',
+                    date: '2020/01/10 15:50:00',
                     message: 'Grazie per avermelo ricordato, le scrivo subito!',
                     status: 'received'
                 }
@@ -161,17 +163,17 @@ createApp({
             visible: true,
             messages: [
                 {
-                    date: '10/01/2020 15:30:55',
+                    date: '2020/01/10 15:30:55',
                     message: 'Ciao, andiamo a mangiare la pizza stasera?',
                     status: 'received'
                 },
                 {
-                    date: '10/01/2020 15:50:00',
+                    date: '2020/01/10 15:50:00',
                     message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
                     status: 'sent'
                 },
                 {
-                    date: '10/01/2020 15:51:00',
+                    date: '2020/01/10 15:51:00',
                     message: 'OK!!',
                     status: 'received'
                 }
@@ -186,12 +188,12 @@ createApp({
     filteredContacts: [], // Array per i contatti filtrati
  
     newListMessage: {
-      date: '10/01/2020 15:50:00',
+      date: '2020/01/10 15:50:00',
       message: '',
       status: 'sent',
     },
     newListAnswerMessage: {
-      date: '10/01/2020 15:50:00',
+      date: '2020/01/10 15:50:00',
       message: '',
       status: 'received',
     },
@@ -216,7 +218,7 @@ createApp({
     onEnter() {
       const newMessage = {
 
-        date: '10/01/2020 15:50:00',
+        date: '2020/01/10 15:50:00',
         message: this.newListMessage.message,
         status: 'sent',
 
@@ -230,7 +232,7 @@ createApp({
 
           const newAnswerMessage = {
 
-            date: '10/01/2020 15:50:00',
+            date: '2020/01/10 15:50:00',
             message: 'ok',
             status: 'received',
     
